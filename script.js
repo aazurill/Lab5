3de96516ee1ea9cda82cffe047178d7ff96fc14c
edwardddeleon <53798230+edwardddeleon@users.noreply.github.com>
2021-05-02 01:12:05 -0700

    Finished clear and volume level; still need to bugfix read text

--- script.js
+++ script.js
@@ -12,6 +12,18 @@ const voices = document.getElementById('voice-selection');
 const generate = document.querySelector('button[type=submit');
 
 const canvas = document.getElementById('user-image');
+
+// Grab volume image
+var volImage = document.getElementById('volume-img');
+// Grab slider
+var slider = document.getElementById('volume-slider');
+
+// Grab reference to SpeechSynthesis controller
+const synth = window.speechSynthesis;
+
+// Create variable for keeping track of volume
+var volume = 100;
+
 // Listens for file upload
 imageUpload.addEventListener('change', () => {
   let file = document.querySelector('input[type=file]').files[0];
@@ -63,6 +75,60 @@ img.addEventListener('load', () => {
   // - If you draw the image to canvas here, it will update as soon as a new image is selected
 });
 
+// Listens for change in slider value, updates volume and changes icons
+slider.onchange = function() {
+  volume = slider.value;
+
+  if(volume >= 67 && volume <= 100) {
+    volImage.src = "icons/volume-level-3.svg";
+  } else if(volume >= 34 && volume <= 66) {
+    volImage.src = "icons/volume-level-2.svg";
+  } else if(volume >= 1 && volume <= 33) {
+    volImage.src = "icons/volume-level-1.svg";
+  } else {
+    volImage.src = "icons/volume-level-0.svg";
+  }
+};
+
+
+// Listens for clear -> clears image/text/input and toggles buttons
+clear.addEventListener('click', () => {
+
+  // Remove image and text from Canvas
+  let ctx = canvas.getContext('2d');
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+  // Toggle clear and read text buttons
+  clear.disabled = true;
+  readText.disabled = true;
+
+  // Clear file name from input
+  document.querySelector('input[type=file]').value = '';
+
+  // Clear top and bottom text
+  document.getElementById('text-top').value = '';
+  document.getElementById('text-bottom').value = '';
+});
+
+// Listens for readText -> changes voice, volume, and reads out text
+readText.addEventListener('click', () => {
+  // Create utterances
+  let botTextVoice = new SpeechSynthesisUtterance(document.getElementById('text-bottom').value);
+  let topTextVoice = new SpeechSynthesisUtterance(document.getElementById('text-top').value);
+
+  // Change voice
+  botTextVoice.voice = voices.getAttribute('lang');
+  topTextVoice.voice = voices.getAttribute('lang');
+
+  // Change volume
+  botTextVoice.volume = volume / 100;
+  topTextVoice.volume = volume / 100;
+
+  // Speak utterances
+  synth.speak(botTextVoice);
+  synth.speak(topTextVoice);
+});
+
 /**
  * Takes in the dimensions of the canvas and the new image, then calculates the new
  * dimensions of the image so that it fits perfectly into the Canvas and maintains aspect ratio
@@ -101,4 +167,34 @@ function getDimmensions(canvasWidth, canvasHeight, imageWidth, imageHeight) {
   }
 
   return { 'width': width, 'height': height, 'startX': startX, 'startY': startY }
+}
+
+function addVoices() {
+  // get list of voices
+  var voiceList = speechSynthesis.getVoices();
+
+  // iterate through each voice
+  for(var i = 0; i < voiceList.length; i++) {
+    var newVoice = document.createElement('option');
+    newVoice.textContent = voiceList[i].name + ' (' + voiceList[i].lang + ')';
+
+    // add text signifying default
+    if(voiceList[i].default) {
+      newVoice.textContent += ' -- DEFAULT';
+    }
+
+    // add attributes to use late
+    newVoice.setAttribute('lang', voiceList[i].lang);
+    newVoice.setAttribute('name', voiceList[i].name);
+
+    // add voice to dropdown
+    voices.appendChild(newVoice);
+  }
+}
+
+// Add voices to the voice dropdown
+addVoices();
+
+if (typeof speechSynthesis !== 'undefined' && speechSynthesis.onvoiceschanged !== undefined) {
+  speechSynthesis.onvoiceschanged = addVoices;
 }
